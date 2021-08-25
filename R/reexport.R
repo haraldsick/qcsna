85aac2daadaae6411b0613180c0a42be52f42d4b
haraldsick <sickharald@posteo.de>
2021-08-25 13:52:06 +0200

    reexports added

--- R/reexport.R
+++ R/reexport.R
@@ -1,6 +1,29 @@
 # sna reexport --------------------
 
+#' @references Butts, Carter T. (2020). sna: Tools for Social Network Analysis. R package
+#' version 2.6. \url{https://CRAN.R-project.org/package=sna}.
+#'
+#' @importFrom sna betweenness
+#' @export
+sna::betweenness
+
+#' @importFrom sna closeness
+#' @export
+sna::closeness
+
 #' @importFrom sna degree
 #' @export
 sna::degree
+
+#' @importFrom sna stresscent
+#' @export
+sna::stresscent
+
+#' @importFrom sna graphcent
+#' @export
+sna::graphcent
+
+#' @importFrom sna evcent
+#' @export
+sna::evcent
 
